d6f5a28ead3648bb8200801b6ac93db8209434c4
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2021-09-14 20:31:12 +0200

    Rewrite in csharp
    I decided to rewrite this yet again, this time in c#. Go lacked a library for writing tags or easy to use ffmpeg wrapper.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loudgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
